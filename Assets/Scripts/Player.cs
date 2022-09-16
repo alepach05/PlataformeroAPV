e049487be9795fca964b88b5d4917a24ec9b3328
Alejandro Pacheco Vargas <a.pacheco.v05@gmail.com>
2022-09-15 19:15:04 -0600

    Jump & Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,20 @@ public class Player : MonoBehaviour
     private float horizontal;
     
     public float speed;
+
+    public float jumpForce;
+
+    private bool isGrounded;
     
     private Rigidbody2D rigidBody2D;
+
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody2D = GetComponent<Rigidbody2D>(); 
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,12 +29,35 @@ public class Player : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal") * speed;
         if(horizontal < 0.0f)
         {
-            transform.localScale = new Vector2(-1.0f, 1.0f);
+            transform.localScale = new Vector2(-20.0f, 20.0f);
         }
         else if(horizontal > 0.0f)
         {
-            transform.localScale = new Vector2(1.0f, 1.0f);
+            transform.localScale = new Vector2(20.0f, 20.0f);
         }
+
+        animator.SetBool("isRunning", horizontal != 0.0f);
+
+        Debug.DrawRay(transform.position, Vector2.down * 1.1f, Color.blue);
+
+        if (Physics2D.Raycast(transform.position, Vector2.down, 1.1f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidBody2D.AddForce(Vector2.up * jumpForce);
     }
 
     private void FixedUpdate()
